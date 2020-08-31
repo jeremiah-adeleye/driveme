06be99b18d48776aabc6b4f6d4f8fdb3bd4bd872
Festus Agboma <festusyuma@gmail.com>
2020-08-31 13:53:01 +0100

    register form error

--- resources/views/user/register.blade.php
+++ resources/views/user/register.blade.php
@@ -21,27 +21,42 @@
             {{csrf_field()}}
             <div class="form-group custom" id="first-name-input" >
                 <label for="first-name">First Name</label>
-                <input type="text" class="form-control input-custom-primary" id="first-name" name="first_name" aria-describedby="firstName">
+                <input type="text" class="form-control input-custom-primary" id="first-name" name="first_name" aria-describedby="firstName" value="{{  old('first_name') }}">
+                @error('first_name')
+                <small class="text-danger" >{{ $message }}</small>
+                @enderror
             </div>
 
             <div class="form-group custom" id="last-name-input" >
                 <label for="last-name">Last Name</label>
-                <input type="text" class="form-control input-custom-primary" id="last-name" name="last_name" aria-describedby="lastName">
+                <input type="text" class="form-control input-custom-primary" id="last-name" name="last_name" aria-describedby="lastName" value="{{  old('last_name') }}">
+                @error('last_name')
+                <small class="text-danger" >{{ $message }}</small>
+                @enderror
             </div>
 
             <div class="form-group custom" id="email-input" >
                 <label for="email">Email Address</label>
-                <input type="email" class="form-control input-custom-primary" id="email" name="email" aria-describedby="email">
+                <input type="email" class="form-control input-custom-primary" id="email" name="email" aria-describedby="email" value="{{  old('email') }}">
+                @error('email')
+                <small class="text-danger" >{{ $message }}</small>
+                @enderror
             </div>
 
             <div class="form-group custom" id="phone-number-input" >
                 <label for="phone-number">Phone Number</label>
-                <input type="text" class="form-control input-custom-primary" id="phone-number" name="phone_number" aria-describedby="phoneNumber">
+                <input type="text" class="form-control input-custom-primary" id="phone-number" name="phone_number" aria-describedby="phoneNumber" value="{{  old('phone_number') }}">
+                @error('phone_number')
+                <small class="text-danger" >{{ $message }}</small>
+                @enderror
             </div>
 
             <div class="form-group custom" id="password-input" >
                 <label for="password">Password</label>
-                <input type="password" class="form-control input-custom-primary" id="password" name="password" aria-describedby="password">
+                <input type="password" class="form-control input-custom-primary" id="password" name="password" aria-describedby="password" value="{{  old('password') }}">
+                @error('password')
+                <small class="text-danger" >{{ $message }}</small>
+                @enderror
             </div>
 
             <div class="form-group custom" id="register-button" >
