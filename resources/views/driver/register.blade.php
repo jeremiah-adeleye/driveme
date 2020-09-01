ee4e9370785c5831b8a8a46aca43ae4b23dab864
Festus Agboma <festusyuma@gmail.com>
2020-09-01 10:47:20 +0100

    driver cache registration input

--- resources/views/driver/register.blade.php
+++ resources/views/driver/register.blade.php
@@ -46,7 +46,7 @@
             <div class="row" >
                 <div class="form-group custom col-md-6" id="first-name-input" >
                     <label for="first-name">First Name</label>
-                    <input type="text" class="form-control input-custom-primary" id="first-name" name="first_name" aria-describedby="firstName">
+                    <input type="text" class="form-control input-custom-primary" id="first-name" name="first_name" aria-describedby="firstName" value="{{ old('first_name') }}" >
                     @error('first_name')
                     <small class="text-danger" >{{ $message }}</small>
                     @enderror
@@ -54,7 +54,7 @@
 
                 <div class="form-group custom col-md-6" id="last-name-input" >
                     <label for="last-name">Last Name</label>
-                    <input type="text" class="form-control input-custom-primary" id="last-name" name="last_name"  aria-describedby="lastName">
+                    <input type="text" class="form-control input-custom-primary" id="last-name" name="last_name"  aria-describedby="lastName" value="{{ old('last_name') }}" >
                     @error('last_name')
                     <small class="text-danger" >{{ $message }}</small>
                     @enderror
@@ -62,7 +62,7 @@
 
                 <div class="form-group custom col-md-6" id="email-input" >
                     <label for="email">Email Address</label>
-                    <input type="email" class="form-control input-custom-primary" id="email" name="email" aria-describedby="email">
+                    <input type="email" class="form-control input-custom-primary" id="email" name="email" aria-describedby="email" value="{{ old('email') }}" >
                     @error('email')
                     <small class="text-danger" >{{ $message }}</small>
                     @enderror
@@ -70,7 +70,7 @@
 
                 <div class="form-group custom col-md-6" id="phone-number-input" >
                     <label for="phone-number">Phone Number</label>
-                    <input type="tel" class="form-control input-custom-primary" id="phone-number" name="phone_number" aria-describedby="phoneNumber">
+                    <input type="tel" class="form-control input-custom-primary" id="phone-number" name="phone_number" aria-describedby="phoneNumber" value="{{ old('phone_number') }}" >
                     @error('phone_number')
                     <small class="text-danger" >{{ $message }}</small>
                     @enderror
@@ -78,7 +78,7 @@
 
                 <div class="form-group custom col-md-6" id="dob-input" >
                     <label for="dob">Date of Birth</label>
-                    <input type="date" class="form-control input-custom-primary" id="dob" name="dob" aria-describedby="dateOfBirth">
+                    <input type="date" class="form-control input-custom-primary" id="dob" name="dob" aria-describedby="dateOfBirth" value="{{ old('dob') }}" >
                     @error('dob')
                     <small class="text-danger" >{{ $message }}</small>
                     @enderror
@@ -86,7 +86,7 @@
 
                 <div class="form-group custom col-md-6" id="location-input" >
                     <label for="location">Location</label>
-                    <input type="text" class="form-control input-custom-primary" id="location" name="location" aria-describedby="location">
+                    <input type="text" class="form-control input-custom-primary" id="location" name="location" aria-describedby="location" value="{{ old('location') }}" >
                     @error('location')
                     <small class="text-danger" >{{ $message }}</small>
                     @enderror
@@ -94,7 +94,7 @@
 
                 <div class="form-group custom col-md-6" id="location-input" >
                     <label for="salary-range">Salary Range <small>(amount you want to be paid)</small></label>
-                    <input type="text" class="form-control input-custom-primary" id="salary-range" name="salary_range" aria-describedby="salaryRange">
+                    <input type="text" class="form-control input-custom-primary" id="salary-range" name="salary_range" aria-describedby="salaryRange" value="{{ old('salary_range') }}" >
                     @error('salary_range')
                     <small class="text-danger" >{{ $message }}</small>
                     @enderror
@@ -103,11 +103,14 @@
                 <div class="form-group custom col-md-6" id="password-input" >
                     <label for="password">Password</label>
                     <input type="password" class="form-control input-custom-primary" id="password" name="password" aria-describedby="password">
+                    @error('address')
+                    <small class="text-danger" >{{ $message }}</small>
+                    @enderror
                 </div>
 
                 <div class="form-group custom col-md-6" id="address-input" >
                     <label for="address">Residential Address <small>(home address)</small></label>
-                    <input type="text" class="form-control input-custom-primary" id="address" name="address" aria-describedby="address">
+                    <input type="text" class="form-control input-custom-primary" id="address" name="address" aria-describedby="address" value="{{ old('address') }}" >
                     @error('address')
                     <small class="text-danger" >{{ $message }}</small>
                     @enderror
@@ -115,7 +118,7 @@
 
                 <div class="form-group custom col-md-6" id="licence-number-input" >
                     <label for="licence-number">Licence Number</label>
-                    <input type="text" class="form-control input-custom-primary" id="licence-number" name="licence_number" aria-describedby="licenceNumber">
+                    <input type="text" class="form-control input-custom-primary" id="licence-number" name="licence_number" aria-describedby="licenceNumber" value="{{ old('licence_number') }}" >
                     @error('licence_number')
                     <small class="text-danger" >{{ $message }}</small>
                     @enderror
@@ -123,7 +126,7 @@
 
                 <div class="form-group custom col-md-6" id="experience-input" >
                     <label for="experience">Driver’s Experience Years</label>
-                    <input type="number" class="form-control input-custom-primary" id="experience" name="experience" aria-describedby="experience">
+                    <input type="number" class="form-control input-custom-primary" id="experience" name="experience" aria-describedby="experience" value="{{ old('experience') }}" >
                     @error('experience')
                     <small class="text-danger" >{{ $message }}</small>
                     @enderror
@@ -131,7 +134,7 @@
 
                 <div class="form-group custom col-md-6" id="vehicle-type-input" >
                     <label for="vehicle-type">What kind of Vehicle do you drive?</label>
-                    <input type="text" class="form-control input-custom-primary" id="vehicle-type" name="vehicle_type" aria-describedby="carType">
+                    <input type="text" class="form-control input-custom-primary" id="vehicle-type" name="vehicle_type" aria-describedby="carType" value="{{ old('vehicle_type') }}" >
                     @error('vehicle_type')
                     <small class="text-danger" >{{ $message }}</small>
                     @enderror
@@ -140,7 +143,7 @@
                 <div class="form-group custom col-md-6 dv-image-input" id="cv-input-number-input" >
                     <label for="cv">Upload Your CV</label><br>
                     <label for="passport" class="input-label" data-target="#cv" ></label>
-                    <input type="file" id="cv" name="cv" aria-describedby="cv" >
+                    <input type="file" id="cv" name="cv" aria-describedby="cv" value="{{ old('cv') }}" >
                     @error('cv')
                     <small class="text-danger" >{{ $message }}</small>
                     @enderror
@@ -149,7 +152,7 @@
                 <div class="form-group custom col-md-6 dv-image-input" id="passport-input" >
                     <label for="passport">Passport Photography</label><br>
                     <label for="passport" class="input-label" data-target="#passport" ></label>
-                    <input type="file" id="passport" name="passport" aria-describedby="passport">
+                    <input type="file" id="passport" name="passport" aria-describedby="passport" value="{{ old('passport') }}" >
                     @error('passport')
                     <small class="text-danger" >{{ $message }}</small>
                     @enderror
