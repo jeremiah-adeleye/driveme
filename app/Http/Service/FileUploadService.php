90241d842b1b8b6196a0c999badf39cc4d18f52b
festus agboma <festusyuma@gmail.com>
2020-10-04 17:13:30 +0100

    add image transformation

--- app/Http/Service/FileUploadService.php
+++ app/Http/Service/FileUploadService.php
@@ -7,7 +7,7 @@ namespace App\Http\Service;
 class FileUploadService{
 
     public function cloudinaryUpload($file) {
-        $response = cloudinary()->upload($file->getRealPath());
+        $response = cloudinary()->upload($file->getRealPath(), ['transformation' => 'ar_1:1,c_fill,g_face,h_200,q_100,r_100,w_200']);
         return $response->getSecurePath();
     }
 }
