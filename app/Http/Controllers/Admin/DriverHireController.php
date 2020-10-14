5952d2801102b453f34b35cbcb4b5234608a121d
festus agboma <festusyuma@gmail.com>
2020-10-14 20:30:55 +0100

    fixes

--- app/Http/Controllers/Admin/DriverHireController.php
+++ app/Http/Controllers/Admin/DriverHireController.php
@@ -55,7 +55,7 @@ class DriverHireController extends Controller{
         $hireRequest = DriverHire::find($id);
 
         if ($hireRequest != null) {
-            $hireRequestDriver = $hireRequest->driverHire->where('id', $driverId);
+            $hireRequestDriver = $hireRequest->driverHire->where('driver_id', $driverId);
 
             if ($hireRequestDriver != null && !$hireRequestDriver->approved) {
                 $hireRequestDriver->approved = true;
@@ -73,7 +73,7 @@ class DriverHireController extends Controller{
         $hireRequest = DriverHire::find($id);
 
         if ($hireRequest != null) {
-            $hireRequestDriver = $hireRequest->driverHire->where('id', $driverId);
+            $hireRequestDriver = $hireRequest->driverHire->where('driver_id', $driverId);
 
             if ($hireRequestDriver != null && !$hireRequestDriver->approved && $hireRequest->active) {
                 $hireRequestDriver->active = false;
