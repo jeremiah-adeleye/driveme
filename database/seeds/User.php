2e19a14af2e99a77751e4c61ba8e5cd42c861c22
jerrycaffe@386konsult.com <jeremiah@386konsult.com>
2020-10-19 20:44:22 +0100

    running seed

--- database/seeds/User.php
+++ database/seeds/User.php
@@ -14,10 +14,10 @@ class User extends Seeder
         DB::table('users')->insert([
             'first_name' => "admin",
             'last_name' => 'admin',
-            'phone_number' => '09088492993',
-            'email' => 'admin@amin.com',
+            'phone_number' => '09088492991',
+            'email' => 'Admin@amin.com',
             'role' => '4',
-            'password' => 'Admin123.',
+            'password' => '$2y$10$uWS4NxOSUhKImSbUM4MaYu.utd0tCiunUPYO3.Uybg72lgu4MwCFq',
            
         ]);
     }
