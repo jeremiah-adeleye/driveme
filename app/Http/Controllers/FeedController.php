9cc08bb93af007c331e2ef074c1958e026dc97cb
jerrycaffe@386konsult.com <jeremiah@386konsult.com>
2020-10-28 09:48:35 +0100

    bug fixed

--- app/Http/Controllers/FeedController.php
+++ app/Http/Controllers/FeedController.php
@@ -9,6 +9,8 @@ class FeedController extends Controller
 {
     public function index()
     {
+        // this shows the admin feed
+        // I will still check if the user is an admin before serving this page
         $active = 'admin.feed';
 
         $notifications = Notification::whereSeen(false)->orderBy('created_at', 'desc')->get();
